02f4c774ed6e360dd53f57c0294c0221efa21520
uni20171273 <uni20171273>
2021-03-31 00:59:46 +0900

    ty

---
+++ run.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+git clone https://github.com/20171119/project.git
+cd project/project1
+mvn install
+mvn assembly:assembly
+java -cp target/cse364-project-1.0-SNAPSHOT-jar-with-dependencies.jar project1.ReadData "Comedy" "artist"
