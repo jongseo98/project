67302f3eabf4616059f0e17292c00ed205559d2e
woojin7879 <woojin7879@unist.ac.kr>
2021-03-31 23:52:40 +0900

    final

--- project1/src/main/java/project1/ReadData.java
+++ project1/src/main/java/project1/ReadData.java
@@ -6,6 +6,7 @@ import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.IOException;
 import java.util.ArrayList;
+import java.util.Arrays;
 
 public class ReadData {
 
@@ -16,19 +17,15 @@ public class ReadData {
         "tradesman/craftsman", "unemployed", "writer"
     };
 
-    // public String OccupToNum(String occup) {
-    //     String num="";
-    //     if ()
-    //     return num;
-    // }
+    public static final String[] movie_genre_list = new String[]{
+        "Action", "Adventure", "Animation", "Children's", "Comedy", "Crime", "Documentary", "Drama", "Fantasy", 
+        "Film-Noir", "Horror", "Musical", "Mystery", "Romance", "Sci-Fi", "Thriller", "War", "Western"
+    };
+
     public static void main(String[] args){
         try{
             String movie_input = args[0];
             String occup_input = args[1];
-            System.out.println(movie_input);
-            System.out.println(occup_input);
-            // System.out.println("movie: " + movie_input + " occup: " + occup_input);
-            // String input = input.replace(" ", ""); // Input 공백 제거
 
             //파일 객체 생성
             File moviefile = new File("/root/project/project1/data/ml-1m/movies.dat");
@@ -39,20 +36,26 @@ public class ReadData {
             BufferedReader ratingbuf = new BufferedReader(new FileReader(ratingfile));
             BufferedReader userbuf = new BufferedReader(new FileReader(userfile));
 
-            // MovieID::Title::Genres - movie.dat
-            // movie 파일 읽기
             String movieline = "";
             ArrayList<String> movieIds = new ArrayList<String>();
+
+            boolean exist_genre = false; // input 값의 movie 장르가 없는경우
 
             while((movieline = moviebuf.readLine()) != null){
                 String[] moviewords = movieline.split("::");
                 String moviegenre = moviewords[2];
                 String[] genres = moviegenre.split("\\|"); // 특수문자 | 는 앞에 \\가 붙어야 구분가능
 
-                if (movie_input.contains("+")) {
-                    String[] movie_inputs = movie_input.split("\\+");
+                if (movie_input.contains("|")) {
+                    String[] movie_inputs = movie_input.split("\\|");
                     boolean flag = false;
                     for (String input: movie_inputs) {
+                        if (Arrays.asList(movie_genre_list).contains(input))
+                            exist_genre = true;
+                        else {
+                            System.out.println("Error. This input can't be aceepted. Try again");
+                            return;
+                        }
                         flag = false;
                         for (String s: genres) {
                             if (input.equalsIgnoreCase(s)) { 
@@ -68,6 +71,12 @@ public class ReadData {
                         movieIds.add(movieId);
                     }
                 } else {
+                    if (Arrays.asList(movie_genre_list).contains(movie_input))
+                            exist_genre = true;
+                    else {
+                        System.out.println("Error. This input can't be aceepted. Try again");
+                        return;
+                    }
                     for (String s: genres) {
                         if (movie_input.equalsIgnoreCase(s)) { // 대소문자 상관없이 input 값과 비교
                             String movieId = moviewords[0];
@@ -78,9 +87,6 @@ public class ReadData {
                 }
             }
 
-            // UserID::Gender::Age::Occupation::Zip-code - user.dat
-            // *  1:  "academic/educator"
-	        // *  2:  "artist"
             String userline = "";
             ArrayList<String> userIds = new ArrayList<String>();
 
@@ -88,7 +94,6 @@ public class ReadData {
                 String[] userwords = userline.split("::");
                 String userId = userwords[0];
                 int userOccup = Integer.parseInt(userwords[3]);
-                // occup_input = occup_input.toLowerCase();
                 if (occup_list[userOccup].contains("/")) {
                     String[] occups = occup_list[userOccup].split("/");
                     for (String s: occups) {
@@ -105,7 +110,6 @@ public class ReadData {
                     userIds.add(userId);
             }
 
-            // UserID::MovieID::Rating::Timestamp - rating.dat
             double rating_sum = 0;
             double rating_num = 0;
 
@@ -113,21 +117,17 @@ public class ReadData {
             while((ratingline = ratingbuf.readLine()) != null){
                 String[] ratingwords = ratingline.split("::");
                 if (userIds.contains(ratingwords[0]) && movieIds.contains(ratingwords[1])) {
-                    // System.out.println("userId: " + ratingwords[0] + " movieId: "+ ratingwords[1] + " rating: " + ratingwords[2]);
                     rating_sum += Double.parseDouble(ratingwords[2]);
                     rating_num += 1;
                 }
             }
 
-            System.out.println("rating_sum: " + rating_sum); 
-            System.out.println("num: " + rating_num);
             double rating_avg = rating_sum/rating_num;
-            if (rating_sum == 0 || rating_num == 0)
-                System.out.println("rating: " + 0);
+            if (rating_num == 0.0 && exist_genre)
+                System.out.println(0);
             else  
-                System.out.println("rating: " + rating_avg);    
-
-            //.readmovieline()은 끝에 개행문자를 읽지 않는다.            
+                System.out.println(rating_avg);
+            
             moviebuf.close();
             ratingbuf.close();
             userbuf.close();
