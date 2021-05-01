3fdf036e45f2ed68afead665b16a6aade6a03c2b
jspark98 <jspark98@unist.ac.kr>
2021-05-01 22:49:28 +0900

    upload

--- project1/run.sh
+++ project1/run.sh
@@ -1,5 +1,10 @@
 #!/bin/bash
 git clone https://github.com/20171119/project.git
+
 cd project1
+
 mvn install
-java -cp target/cse364-project-1.0-SNAPSHOT-jar-with-dependencies.jar project1.ReadData "adventure" "artist"
+
+mvn assembly:assembly
+
+java -cp target/cse364-project-1.0-SNAPSHOT-jar-with-dependencies.jar project1.ReadData adventure artist
