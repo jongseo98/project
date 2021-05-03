b1ec003fc49029ec2cefefde07553ba3df8da8f2
uni20171273 <38204494+uni20171273@users.noreply.github.com>
2021-05-03 17:56:30 +0900

    Update run.sh

--- run.sh
+++ run.sh
@@ -2,14 +2,12 @@
 
 git clone https://github.com/20171119/project.git
 
-cd project2
-
-mvn clean
+cd project/project2
 
 mvn install
 
 mvn assembly:assembly
 
-mvn jacoco:prepare-agent test jacoco:report
+java -cp target/cse364-project-1.0-SNAPSHOT-jar-with-dependencies.jar project2.Recommend "M" "25" "Artist"
 
 java -cp target/cse364-project-1.0-SNAPSHOT-jar-with-dependencies.jar project2.Recommend "F" "25" "Gradstudent" "Action|Comedy"
