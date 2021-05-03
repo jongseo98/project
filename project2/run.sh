6fc5db1fa7e9ccdba9e46fe8f1760770b82c1249
jspark98 <jspark98@unist.ac.kr>
2021-05-03 19:50:42 +0900

    Real Finish

--- project2/run.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-git clone https://github.com/20171119/project.git
-cd project/project2
-mvn install
-mvn assembly:assembly
-java -cp target/cse364-project-1.0-SNAPSHOT-jar-with-dependencies.jar project2.Recommend "" "" ""
-java -cp target/cse364-project-1.0-SNAPSHOT-jar-with-dependencies.jar project2.Recommend "" "" "" "Adventure"
