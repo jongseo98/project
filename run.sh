488373fb80d67e28a8a407c0221c26d8ec79bddc
20171119 <jspark98@unist.ac.kr>
2021-05-25 12:35:09 +0000

    Finish

--- run.sh
+++ run.sh
@@ -1,8 +1,4 @@
 #!/bin/bash
 git clone https://github.com/20171119/project.git
 cd project/project3
-<<<<<<< HEAD
 mvn spring-boot:run
-=======
-mvn spring-boot:run
->>>>>>> c49af1dd6cd3c162c240460794abb1d9415ebbae
